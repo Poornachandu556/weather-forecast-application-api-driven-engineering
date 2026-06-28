011ba0b903dab958af0b1d6dc62b79cb840d38b5
poorna chandu <poornaroyal47@gmail.com>
2026-06-28 13:06:33 +0530

    Set base path for weather forecast applicationupdate

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/weather-forecast-application-api-driven-engineering/',
   plugins: [react()],
   server: {
     port: 5173,
